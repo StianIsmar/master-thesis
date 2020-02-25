fixed resampling from radians to rotations

src/data_processing/ff_transform.py
@@ -83,9 +83,9 @@ class FastFourierTransform:
             plt.xlabel("Order [X]")
             y = np.abs(fft)[:N // 2] * 1 /N # Normalized. Cutting away half of the fft frequencies.
             plt.plot(f, y, markersize=0.5, marker="o", lw=2)
-            plt.axvline(x=4.5, c='g', linewidth=1)
-            plt.axvline(x=8.9, c='r', linewidth=1)
-            plt.axvline(x=13.4, c='y', linewidth=1)
+            plt.axvline(x=28, c='r', linewidth=0.7)
+            plt.axvline(x=56, c='r', linewidth=0.7)
+            plt.axvline(x=84, c='r', linewidth=0.7)
             plt.axhline(y=0.2, c='y', linewidth=1)
 
             #plt.xticks(range(1, len(xticks) + 1), f, rotation=90)

src/data_processing/resample.py
@@ -54,8 +54,8 @@ def linear_interpolation_resampling(time_stamps, vibration_signal, peak_array, n
 
         # Transform the x values from time domain to radians domain
         delta_x = resampled_x[1] - resampled_x[0]
-        round_start = 2*np.pi * i
-        round_end = 2*np.pi * i + 2*np.pi - delta_x
+        round_start = i #2*np.pi * i
+        round_end = i + 1 - delta_x #2*np.pi * i + 2*np.pi - delta_x
         X_values_round_domain = np.linspace(round_start, round_end, number_of_resample_points)
 
         # Collect the transformed lists
@@ -123,7 +123,7 @@ def linear_interpolation_resampling(time_stamps, vibration_signal, peak_array, n
         plt.figure(figsize=(15, 5))
         plt.plot(x_resampled, y_resampled, linewidth=0.2)
         plt.title(f'Resampled Vibration Data: {name} \nNumber of Data Points: {x_resampled.shape[0]}')
-        plt.xlabel('Rounds (in radians)', fontsize=16)
+        plt.xlabel('Rotations', fontsize=16)
         plt.ylabel('Vibration amplitude (in m/s2', fontsize=16)
         for i, round_value in enumerate(resampled_vertical_lines):
             plt.axvline(x=round_value, c='r', linewidth=0.8)
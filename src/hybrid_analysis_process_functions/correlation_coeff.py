52870fcc51c26d1bf030f1f4f13f93b662583c2b
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2020-03-24 15:28:35 +0100

    Tweaked functions

--- src/hybrid_analysis_process_functions/correlation_coeff.py
+++ src/hybrid_analysis_process_functions/correlation_coeff.py
@@ -9,8 +9,8 @@ def get_corr_coef(signals):
 	matrix = np.corrcoef(signals)
 	amount_imfs = len(signals) - 1
 	imf_labels = [f'IMF{i+1}' for i in range(amount_imfs)]
-	imf_labels.insert(0,'Raw signal',)
-	df = pd.DataFrame(np.corrcoef(signals))
+	imf_labels.insert(0,'Raw signal')
+	df = pd.DataFrame(data=matrix)
 	df.insert(loc=0,value=imf_labels, column='Signal')
 	df = df.drop(columns=np.arange(1,len(signals)),axis=1)
 	df = df.rename(columns={0:'Correlation coefficient'})

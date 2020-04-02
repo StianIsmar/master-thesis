600379e032d6c5940457a3efb2168644e8945cf8
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2020-04-02 17:45:38 +0200

    Implemented 2 features from the bispectrum

--- src/utils/feature_extraction.py
+++ src/utils/feature_extraction.py
@@ -2,6 +2,15 @@ import numpy as np
 from scipy.stats import kurtosis, skew
 from scipy import signal
 
+# bispectrum
+from math import pi
+import numpy as np
+from numpy.fft import rfftfreq, rfft
+from scipy.fftpack import next_fast_len
+from scipy.signal import spectrogram
+import matplotlib.pyplot as plt
+
+
 def get_time_domain_features(sig,fs):
     # rms
     r = sum(map(lambda x: x**2,sig))
@@ -21,3 +30,14 @@ def get_time_domain_features(sig,fs):
     signal_mean = np.mean(sig)
     
     return rms, kurt, skewness, energy_mean, signal_mean
+
+
+
+def get_freq_domain_features(sig,fs,N=5000):
+'''
+    Using bispectrum.
+'''
+# Select the N to split the signal into N parts
+kw = dict(nperseg=N // 10, noverlap=N // 20, nfft=next_fast_len(N // 2))
+freq1, fre2, bispec = polycoherence.polycoherence(s, fs, norm=None, **kw)
+polycoherence.plot_polycoherence(freq1, fre2, bispec)

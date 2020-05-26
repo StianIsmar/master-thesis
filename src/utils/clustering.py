c9d9c3c0954d73ec879cbbec525135e79b88263f
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2020-05-26 12:59:00 +0200

    Adjustments to clustering plots

--- src/utils/clustering.py
+++ src/utils/clustering.py
@@ -30,7 +30,7 @@ ROOT_PATH = os.path.abspath(".").split("src")[0]
 
 plt.style.use('file://' + ROOT_PATH + "src/utils/plotparams.rc")
 
-def plot_clusters(y_cluster_kmeans,feature_df,plot=True):
+def plot_clusters(wt_num,k,y_cluster_kmeans,feature_df,plot=True,dataset_name=''):
     '''
     Args:
         y_cluster_kmeans (List): List of all datapoints and which cluster they are assigned to.
@@ -39,8 +39,8 @@ def plot_clusters(y_cluster_kmeans,feature_df,plot=True):
         clusterDict: (dictionary) Dictionary with keys=clusters and values=indexes. 
     '''
     def map_index(index):
-        return feature_df.iloc[index,:]['Index']
-
+        result=feature_df.iloc[index,:]['Index']
+        return result
 
     myDict = {}
     min_clus = y_cluster_kmeans.min()
@@ -51,7 +51,6 @@ def plot_clusters(y_cluster_kmeans,feature_df,plot=True):
         elif min_clus > 0:
             y_cluster_kmeans = y_cluster_kmeans - min_clus
         min_clus = 0
-    
     # Placing the clustered points in cluster lists (where the lists in each key contain indexes)
     for i, elem in enumerate(y_cluster_kmeans):
         if (elem in myDict):
@@ -61,7 +60,7 @@ def plot_clusters(y_cluster_kmeans,feature_df,plot=True):
 
     for i in range(min_clus,len(myDict)): # Sorting the dictionaries
         np.sort(myDict[i])
-    
+
     for i in range(min_clus,len(myDict)): # map these indexes to the actual indexes in the interval.
         index_list=myDict[i]
         new_list=[]
@@ -75,7 +74,7 @@ def plot_clusters(y_cluster_kmeans,feature_df,plot=True):
 
     if plot:
         fig, axs = plt.subplots(1,len(myDict), figsize=(15, 6), facecolor='w', edgecolor='k',sharex=True, sharey=True)
-        fig.suptitle("Cluster assignment",fontsize=14) # change location
+        fig.suptitle(f"WT0{wt_num}: Cluster assignment", fontsize=14) # change location
         plt.grid(b=None)
 
 
@@ -100,6 +99,10 @@ def plot_clusters(y_cluster_kmeans,feature_df,plot=True):
         cbar = plt.colorbar(sm,cax=cax)
         cbar.set_label('Interval index', rotation=270,labelpad=15)
 
+        # plt.tight_layout()
+
+        save_path=f'../../plots/cluster_results'
+        plt.savefig(f'{save_path}/wt{wt_num}kmeans_{k}_clusters_{dataset_name}.png',dpi=300)
 
         plt.show()
 
@@ -125,7 +128,10 @@ def find_optimal_dist(X):
         plt.grid(True)
 from sklearn.cluster import DBSCAN
 
-def k_means_clustering(df,kind,knn_clusters=8,pca_components=10,plot=True):
+
+
+
+def k_means_clustering(wt_num,df,kind,knn_clusters=8,pca_components=10,plot=True,dataset_name=''):
     res = df
     cluster_df=res
     try:
@@ -139,8 +145,26 @@ def k_means_clustering(df,kind,knn_clusters=8,pca_components=10,plot=True):
         pass
     scaled = scale_df(cluster_df)
 
-    def dfScatter(df, xcol='Height', ycol='Weight', catcol='Gender'):
-        fig, ax = plt.subplots(figsize=(15,4))
+    def map_index1(i,df,labels):
+        try:
+            i=i.replace("−", "-")
+        except:
+            print('Did not replace')
+
+        if int(i) < 0:
+            return int(i)
+        if (i==labels[-1]):
+            return ' '
+
+        result=df.iloc[int(i),:]['Index']
+
+        # print(result)
+        # print(i,result)
+        return round(result)
+
+    def dfScatter(feature_df,df, xcol='Height', ycol='Weight', catcol='Gender'):
+
+        fig, ax = plt.subplots(figsize=(15,5))
         # cmap=plt.get_cmap('tab20c')
         cmap = plt.get_cmap('Dark2')
 
@@ -151,10 +175,34 @@ def k_means_clustering(df,kind,knn_clusters=8,pca_components=10,plot=True):
             colordict[i+1] = cmap(colordict[i+1])
         df["Color"] = df[catcol].apply(lambda x: colordict[x])
         # df['Color'] = sns.color_palette("deep",len(colordict))
-        ax.scatter(df[xcol], df[ycol], c=df.Color, s=40)
+        ax.scatter(df[xcol], df[ycol], c=df.Color, s=50)
+
+        first = df[xcol].values[0]
+        last = df[xcol].values[-1]
+            
+        old_range = np.arange(first,last,20)
+
+        last_transformed = map_index1(last,feature_df,[-20])
+
+        new_labels1 = [int(round(i)) for i in (np.arange(first,last_transformed,40))]
+
         axes = plt.gca()
         axes.yaxis.grid(True)
         axes.xaxis.grid(False)
+
+        plt.draw()
+
+        ax.set_xticks(np.linspace(old_range[0],old_range[-1],len(new_labels1)))
+        ax.set_xticklabels(new_labels1)
+
+        '''
+        for i in range(1,len(labels)):
+            print(labels[i])
+            labels[i] = map_index(labels[i],feature_df)
+
+        '''
+        # print(labels)
+
         return fig,ax
 
     if type=='kmeans_pca':
@@ -171,14 +219,25 @@ def k_means_clustering(df,kind,knn_clusters=8,pca_components=10,plot=True):
 
     wcss = []
     if plot:
-        for i in range(1, 11):
+        fig=plt.figure(figsize=(15,4))
+        for i in range(1, knn_clusters+1):
             kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
             kmeans.fit(X)
-            wcss.append(kmeans.inertia_)
-        plt.plot(range(1, 11), wcss)
-        plt.title('Elbow Method')
+            wcss.append(kmeans.inertia_) # Sum of squared distances of samples to their closest cluster center.
+        plt.plot(range(1, knn_clusters+1), wcss)
+        plt.margins(0)
+        plt.title('WT04: Elbow Method')
         plt.xlabel('Number of clusters')
         plt.ylabel('WCSS')
+
+        locs, labels = plt.yticks()            # Get locations and labels
+        plt.xticks(np.arange(1, knn_clusters+1, 1.0))
+
+        
+        # Saving the elbow plot.
+        save_path=f'../../plots/cluster_results'
+        plt.tight_layout()
+        plt.savefig(f'{save_path}/wt_{wt_num}_{dataset_name}_elbow.png',dpi=300)
         plt.show()
     
     kmeans = KMeans(n_clusters=knn_clusters, init='k-means++', max_iter=300, n_init=10, random_state=0)
@@ -187,6 +246,11 @@ def k_means_clustering(df,kind,knn_clusters=8,pca_components=10,plot=True):
     cluster_map = pd.DataFrame()
     cluster_map['data_index'] = res.index.values
     cluster = pred_y
+
+    # map it here
+    # new_pred_y= [map_index(i,df) for i in pred_y]
+    # print(new_pred_y)
+
     min_clus = cluster.min()
     if min_clus < 0:
         cluster = cluster + (min_clus*-1+1)    
@@ -195,20 +259,39 @@ def k_means_clustering(df,kind,knn_clusters=8,pca_components=10,plot=True):
     elif min_clus == 0:
         cluster = cluster + 1
 
+    # print(cluster)
+
     if plot:
         cluster_map['cluster'] = cluster
-        fig,ax = dfScatter(cluster_map,xcol='data_index',ycol='cluster',catcol='cluster')
-        plt.title("Clustering assignment for each time interval")
-        plt.xlabel("Clusters")
+
+
+        fig,ax = dfScatter(df, cluster_map,xcol='data_index',ycol='cluster',catcol='cluster')
+        plt.title("WT04: Clustering assignment for each signal over time")
+        plt.ylabel("Cluster number")
+
+        plt.xlabel("Data sample index")
         locs, labels = plt.yticks()            # Get locations and labels
-        plt.yticks(np.arange(0, max(cluster)+1, 1.0))
+
+        plt.yticks(np.arange(1, max(cluster)+1, 1.0))
         
         # Some figtext
-        fig.text(0.04, -0.16, 'August\n2018', horizontalalignment='center', verticalalignment='center', transform=ax.transAxes,fontsize=14)
-        fig.text(0.96, -0.16, 'January\n2020', horizontalalignment='center', verticalalignment='center', transform=ax.transAxes,fontsize=14)
+        fig.text(0.04, -0.12, 'August\n2018', horizontalalignment='center', verticalalignment='center', transform=ax.transAxes,fontsize=14)
+        fig.text(0.96, -0.12, 'January\n2020', horizontalalignment='center', verticalalignment='center', transform=ax.transAxes,fontsize=14)
         # ax.set_yticks(np.arange(locs.min(),locs.max()),np.arange(0,(cluster.max()+1)))
+        
+
+        # Saving the cluster development plot
+        plt.tight_layout()
+        save_path=f'../../plots/cluster_results'
+        plt.savefig(f'{save_path}/wt_{wt_num}_wcss_{dataset_name}_dev.png',dpi=300)
         plt.show()
     return pred_y
+
+    '''
+
+    DBSCAN
+
+    '''
   
 def db_scan_clustering(df,kind,eps=0.25,min_samples=5,pca_components=8,knn_clusters=8,plot=True):
     res = df
@@ -224,15 +307,17 @@ def db_scan_clustering(df,kind,eps=0.25,min_samples=5,pca_components=8,knn_clust
     scaled = scale_df(cluster_df)
 
     # Find the optimal epsilon
-
+    def map_index(index):
+        result=feature_df.iloc[index,:]['Index']
+    return result
 
     def dfScatter(df, xcol='Height', ycol='Weight', catcol='Gender'):
         fig, ax = plt.subplots(figsize=(15,4))
         # cmap=plt.get_cmap('tab20c')
+
         cmap=plt.get_cmap('Dark2')
 
         ## sns.palplot(sns.color_palette("deep",20))
-
 
         categories = np.unique(df[catcol])
         colors = np.linspace(0, 1, len(categories))
@@ -275,7 +360,7 @@ def db_scan_clustering(df,kind,eps=0.25,min_samples=5,pca_components=8,knn_clust
         #sns.scatterplot(x=cluster_map['data_index'],y=cluster_map['cluster'],hue=cluster_map['cluster'])
         if plot:
             fig,ax = dfScatter(cluster_map,xcol='real_index',ycol='cluster',catcol='cluster')
-            fig.suptitle("Clustering assignment for each time interval",fontsize=14)
+            fig.suptitle("Cluster assignment for each time interval",fontsize=14)
             plt.xlabel(f"Interval number")
             #plt.grid(axis='y')
             ax.xaxis.grid(False)    
@@ -346,7 +431,7 @@ def db_scan_clustering(df,kind,eps=0.25,min_samples=5,pca_components=8,knn_clust
         return clustering.labels_,principalDf
 
 
-def plot_clusters_pair_plot(df, features, y_clusters):
+def plot_clusters_pair_plot(wt_num,k,cluster_method,df,features,y_clusters,dataset_name=''):
     '''
     df:: The dataframe with all features for wt.
     features:: the featres in the pairplot to be studied. EX: ['ActPower','B1','B4']
@@ -364,16 +449,17 @@ def plot_clusters_pair_plot(df, features, y_clusters):
     min_clus = 0
     df['cluster_assigned'] = y_clusters
     
-
-
     # hue='Index'
     ax=sns.pairplot(df,vars=features, hue='cluster_assigned',palette=sns.color_palette(sns.hls_palette(8, l=.4, s=.8), len(np.unique(y_clusters))))
+    
+    # Saving the cluster development plot
+    plt.tight_layout()
+    save_path=f'../../plots/cluster_results'
+    plt.savefig(f'{save_path}/wt{wt_num}_{cluster_method}_{k}_{dataset_name}_clusters_pairplot.png',dpi=300)
     plt.show()
     #current_palette = sns.color_palette()
     #print(current_palette)
     #sns.palplot(current_palette)
-
-
 
 
 
